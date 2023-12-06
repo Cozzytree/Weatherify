5eef7bdf14ab325709f3c168a89b8f5dced37473
Cozzytree <135008941+Cozzytree@users.noreply.github.com>
2023-12-06 20:43:58 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,5 @@ import vitePluginRequire from "vite-plugin-require";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-build :{target :"next"},
   plugins: [react(), vitePluginRequire.default()],
 });
